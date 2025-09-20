456e928e5c70657cbb5dd5640985fa3e316a851c
varundhangar <varundhangar99775@gmail.com>
2025-09-20 16:17:54 +0530

    Update lodo.py

--- lodo.py
+++ lodo.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import streamlit as st
-import sklearn
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -62,6 +61,7 @@ if st.button("Check Message"):
             
         else:
             st.success("⚠️ This is a FRAUD message.")
+
 
 
 
