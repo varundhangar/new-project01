0fd9a94de6b8977a02904d9991a17f1f2feba7c1
varundhangar <varundhangar99775@gmail.com>
2025-09-20 16:20:38 +0530

    Update lodo.py

--- lodo.py
+++ lodo.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import streamlit as st
 #from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
-from sklearn.feature_extraction.text import TfidfVectorizer
+# sklearn.linear_model import LogisticRegression
+#from sklearn.feature_extraction.text import TfidfVectorizer
 import joblib
 
 #from sklearn.neighbors import KNeighborsClassifier   
@@ -61,6 +61,7 @@ if st.button("Check Message"):
             
         else:
             st.success("⚠️ This is a FRAUD message.")
+
 
 
 
